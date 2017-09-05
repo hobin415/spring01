c99e4f74974fcde2661113fa7cefd03b30f171c7
java <java@bigdata>
2017-09-05 15:07:08 +0900

    java version 1.5 -> 1.8

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -3,8 +3,9 @@ package com.example;
 public class HelloSpring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Spring의 두가지 추상화된 단계");
+		System.out.println("1. DI(Dependency Injection 의존 주입");
+		System.out.println("2. AOP(Aspect Objectd Programming) 관점 지향 프로그래밍");
 	}
 
 }
